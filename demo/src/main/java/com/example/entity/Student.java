fc27d577e27cc2d039071de5be390cce6d9d0436
Sobhan Zadsar <sobhanzadsar@gmail.com>
2025-04-04 12:07:45 +0330

    First commit

---
+++ demo/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,41 @@
+package com.example.entity;
+
+import CreateStudentRequest.CreateStudentRequest;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Student (CreateStudentRequest createStudentRequest ) {
+		this.firstName =createStudentRequest.getFirstName();
+		this.lastName =createStudentRequest.getLastName();
+		this.email =createStudentRequest.getEmail();
+	}
+}
